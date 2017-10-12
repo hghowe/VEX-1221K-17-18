0d1335fafa0a1b0d3471cda4ec13d3aa6a5471cd
Robotics <robotics@us-robotics.kinkaid.org>
2017-10-12 14:39:19 -0500

    Initial Commit

---
+++ Clawbot/src/auto.c
@@ -0,0 +1,78 @@
+/** @file auto.c
+ * @brief File for autonomous code
+ *
+ * This file should contain the user autonomous() function and any functions related to it.
+ *
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * PROS contains FreeRTOS (http://www.freertos.org) whose source code may be
+ * obtained from http://sourceforge.net/projects/freertos/files/ or on request.
+ */
+
+#include "main.h" 
+/*
+ * Runs the user autonomous code. This function will be started in its own task with the default
+ * priority and stack size whenever the robot is enabled via the Field Management System or the
+ * VEX Competition Switch in the autonomous mode. If the robot is disabled or communications is
+ * lost, the autonomous task will be stopped by the kernel. Re-enabling the robot will restart
+ * the task, not re-start it from where it left off.
+ *
+ * Code running in the autonomous task cannot access information from the VEX Joystick. However,
+ * the autonomous function can be invoked from another task if a VEX Competition Switch is not
+ * available, and it can access joystick information if called in this way.
+ *
+ * The autonomous task may exit, unlike operatorControl() which should never exit. If it does
+ * so, the robot will await a switch to another mode or disable/enable cycle.
+ */
+ void moveForward(int power, int timeInterval)
+ {
+   motorSet(1, power);
+   motorSet(10, -power);
+   delay(timeInterval);
+ }
+ void moveForwardUntil(int power)
+ {
+   motorSet(1, power);
+   motorSet(10, -power);
+   while (ultrasonicGet(sonar) > 70)
+   {
+     delay(1);
+     if (!isAutonomous())
+      break;
+   }
+     motorSet(1, 0);
+     motorSet(10, 0);
+
+ }
+ void stopall()
+ {
+   motorSet(1, 0);
+   motorSet(10, 0);
+   motorSet(6, 0);
+   motorSet(7, 0);
+ }
+ void stopMotors()
+ {
+   motorSet(1, 0);
+   motorSet(10, 0);
+ }
+
+void autonomous()
+{
+  // moveForward(127, 1000);
+  // motorSet(7,-127);
+  // delay(700);
+  // stopall();
+  // delay(2500);
+  // motorSet(6,110);
+  // delay(750);
+  // motorSet(6,0);
+  // moveForward(-127,100);
+  // stopall();
+  // delay(500);
+  moveForwardUntil(127);
+}
+
+// printf("Distance is: %d\n",ultrasonicGet(sonar));
+// delay(200);
---
+++ Clawbot/src/opcontrol.c
@@ -0,0 +1,88 @@
+/** @file opcontrol.c
+ * @brief File for operator control code
+ *
+ * This file should contain the user operatorControl() function and any functions related to it.
+ *
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * PROS contains FreeRTOS (http://www.freertos.org) whose source code may be
+ * obtained from http://sourceforge.net/projects/freertos/files/ or on request.
+ */
+
+#include "main.h"
+
+/*
+ * Runs the user operator control code. This function will be started in its own task with the
+ * default priority and stack size whenever the robot is enabled via the Field Management System
+ * or the VEX Competition Switch in the operator control mode. If the robot is disabled or
+ * communications is lost, the operator control task will be stopped by the kernel. Re-enabling
+ * the robot will restart the task, not resume it from where it left off.
+ *
+ * If no VEX Competition Switch or Field Management system is plugged in, the VEX Cortex will
+ * run the operator control task. Be warned that this will also occur if the VEX Cortex is
+ * tethered directly to a computer via the USB A to A cable without any VEX Joystick attached.
+ *
+ * Code running in this task can take almost any action, as the VEX Joystick is available and
+ * the scheduler is operational. However, proper use of delay() or taskDelayUntil() is highly
+ * recommended to give other tasks (including system tasks such as updating LCDs) time to run.
+ *
+ * This task should never exit; it should end with some kind of infinite loop, even if empty.
+ */
+
+
+void operatorControl()
+{
+		bool clawOpen;
+		bool clawClose;
+
+		bool armUp;
+		bool armDown;
+
+		int power;
+	  int turn;
+		int clawPower;
+		int armPower;
+		while (1)
+		{
+					clawOpen = joystickGetDigital(1,5,JOY_UP);
+					clawClose = joystickGetDigital(1,5,JOY_DOWN);
+
+					armUp = joystickGetDigital(1,6,JOY_UP);
+					armDown = joystickGetDigital(1,6,JOY_DOWN);
+
+	        turn = (joystickGetAnalog(1, 4) + joystickGetAnalog(1, 1))/2; // vertical axis on right joystick
+	        power = (joystickGetAnalog(1, 3) + joystickGetAnalog(1, 2))/2; // horizontal axis on right joystick
+
+					clawPower = 0;
+					if (clawOpen == 1)
+					{
+						clawPower = 110;
+					}
+					if (clawClose == 1)
+					{
+						clawPower = clawPower  -110;
+					}
+
+					armPower = 0;
+					if (armDown == 1)
+					{
+						armPower = 110;
+					}
+					if (armUp == 1)
+					{
+						armPower = armPower  -110;
+					}
+
+					motorSet(6,clawPower);
+					motorSet(7,armPower);
+
+					motorSet(1, power + turn);
+					motorSet(10, -1*(power-turn)); // the -1 is because the motor is reversed.
+
+					printf ("Test.");
+					// printf("%d",digitalRead(BUTTON_PORT));
+
+					delay(20);
+		}
+}
